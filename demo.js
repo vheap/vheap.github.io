c9e77da8d4dbe47544f845985dcb8abf70af6679
vheap <45735802+vheap@users.noreply.github.com>
2018-12-22 20:16:54 +0200

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,4 +1,5 @@
-  var name = "grades";
-$.get('http://vastheap.com/' + name, function(response) {
-$("#total").html("Text added by jQuery code.");
+var name = "grades";
+var url = "http://anyorigin.com/go?url=" + encodeURIComponent("http://vastheap.com/") + name + "&callback=?";
+$.get(url, function(response) {
+  $("#total").html("Text added by jQuery code.");
 });
